cf86b0fe9ee6b71ab54ab7acf63917719520d074
bdebot-dev <b.debot@codeur.online>
2022-04-12 22:13:48 +0200

    Verifying form fields

--- handler_project-add.php
+++ handler_project-add.php
@@ -1,5 +1,9 @@
 <?php
 
+// Verifying form fields
+if(isset($_POST['data_title']) && !empty($_POST['data_title'])
+&& isset($_POST['data_description']) && !empty($_POST['data_description'])){
+
     // Data cleaning & storing in variables
     $project_title = strip_tags($_POST['data_title']);
     $project_description = strip_tags($_POST['data_description']);
@@ -15,3 +19,9 @@
     // Redirection
     echo '<div>Project added.</div>';
     echo '<div><a href="index.php"><button>Back</button></a></div>';
+
+//If the form fields are empty
+} else {
+    echo "Complete all fields. ";
+    echo '<div><a href="index.php"><button>Back</button></a></div>';
+ }
