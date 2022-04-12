8a4c985ec27690217ec7eb74a89e0edfe3fb9693
bdebot-dev <b.debot@codeur.online>
2022-04-12 21:28:53 +0200

    Include header & footer

--- db_connection.php
+++ db_connection.php
@@ -9,7 +9,7 @@ try {
     $db = new PDO("mysql:host=$db_servername;dbname=$db_dbname", $db_username, $db_password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    //echo "Connected successfully";
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A great developer portfolio, consisting of a back office and a visitor interface.">
+    <title>Portfolio</title>
+</head>
+<body>
