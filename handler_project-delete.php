e3a43b2c146621fa9bcece1b58f93bfa89e7fa2d
bdebot-dev <b.debot@codeur.online>
2022-04-12 22:41:29 +0200

    Verifying form fields and data cleaning

--- handler_project-delete.php
+++ handler_project-delete.php
@@ -1,10 +1,27 @@
 <?php
 
+// Verifying form fields
+if(isset($_GET['project_id']) && !empty($_GET['project_id'])){
+
     require_once('db_connection.php');
+
+    // Data cleaning & storing in variable
     $project_id = $_GET['project_id'];
+
+    // Delete request
     $sql = 'DELETE FROM `table_projects` WHERE `project_id` = :project_id;';
     $query = $db->prepare($sql);
     $query->bindValue(':project_id', $project_id, PDO::PARAM_INT);
     $query->execute();
+
+    // Success
     echo '<div>Project deleted!</div>';
     echo '<div><a href="index.php"><button>Back</button></a></div>';
+
+//If the form fields are empty    
+} else {
+
+    echo '<div>URL is not valid...</div>'; 
+    echo '<div><a href="home.php"><button>Back</button></a></div>';
+
+}
