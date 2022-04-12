b9cd7484669799ea239358646289e132d10b038b
bdebot-dev <b.debot@codeur.online>
2022-04-12 22:47:58 +0200

    Checking existence of the id sent by url (method GET)

--- handler_project-delete.php
+++ handler_project-delete.php
@@ -1,27 +1,37 @@
 <?php
 
 // Verifying form fields
-if(isset($_GET['project_id']) && !empty($_GET['project_id'])){
+if (isset($_GET['project_id']) && !empty($_GET['project_id'])) {
 
     require_once('db_connection.php');
 
     // Data cleaning & storing in variable
     $project_id = $_GET['project_id'];
 
-    // Delete request
-    $sql = 'DELETE FROM `table_projects` WHERE `project_id` = :project_id;';
+    // Checking existence of the id sent by url (method GET)
+    $sql = 'SELECT * FROM `table_projects` WHERE `project_id` = :project_id;';
     $query = $db->prepare($sql);
     $query->bindValue(':project_id', $project_id, PDO::PARAM_INT);
     $query->execute();
-
-    // Success
-    echo '<div>Project deleted!</div>';
-    echo '<div><a href="index.php"><button>Back</button></a></div>';
+    $result = $query->fetch();
+    if ($result) {
+        // Delete request
+        $sql = 'DELETE FROM `table_projects` WHERE `project_id` = :project_id;';
+        $query = $db->prepare($sql);
+        $query->bindValue(':project_id', $project_id, PDO::PARAM_INT);
+        $query->execute();
+        // Success
+        echo '<div>Project deleted!</div>';
+        echo '<div><a href="index.php"><button>Back</button></a></div>';
+    } else {
+        echo '<div>This ID doesn\'t exist.</div>';
+        echo '<div><a href="index.php"><button>Back</button></a></div>';
+    }
 
 //If the form fields are empty    
 } else {
 
     echo '<div>URL is not valid...</div>'; 
-    echo '<div><a href="home.php"><button>Back</button></a></div>';
+    echo '<div><a href="index.php"><button>Back</button></a></div>';
 
 }
