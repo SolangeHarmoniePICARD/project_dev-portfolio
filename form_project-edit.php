3db9095cf9aa5b66ad14e046eed7365c1afaf3b7
bdebot-dev <b.debot@codeur.online>
2022-04-12 23:10:46 +0200

    Retrieving data from a project in the edit form.

--- form_project-edit.php
+++ form_project-edit.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if (isset($_GET['project_id']) && !empty($_GET['project_id'])) {
     require_once('db_connection.php');
     $project_id = strip_tags($_GET['project_id']);
@@ -26,3 +25,23 @@ if (isset($_GET['project_id']) && !empty($_GET['project_id'])) {
 }
 
 ?>
+
+<?php include 'header.php' ?>
+
+<form action="handler_project-edit.php" method="post">
+    <input type="hidden" name="project_id" value='<?= $result['project_id'] ?>'>
+    <div>
+        <label for="input_title">Title: </label>
+        <input type="text" id="input_title" name="data_title" value="<?=$result['project_title']?>">
+    </div>
+    <div>
+        <label for="input_description">Description: </label>
+        <textarea id="input_description" rows="8" name="data_description"><?=$result['project_description']?></textarea>
+    </div>
+    <div>
+        <input type="submit" id="form_submit" value="Edit Project">
+        <input type="reset" value="Reset">
+    </div>
+</form>
+
+<?php include 'footer.php' ?>
