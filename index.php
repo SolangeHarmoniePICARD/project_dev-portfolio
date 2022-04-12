4858ec78dd7b2d91bc32996b0a1a674b78bfa475
bdebot-dev <b.debot@codeur.online>
2022-04-12 22:23:42 +0200

    Loop on the list of projects

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
+<?php
+    require_once('db_connection.php');
+    $sql = 'SELECT * FROM `table_projects`';
+    $query = $db->prepare($sql);
+    $query->execute();
+    $projects = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <?php include 'header.php' ?>
+
+<div>
+    <?php foreach($projects as $project){ ?>
+        <div>
+            <?= $project['project_title'] ?> 
+        </div>
+    <?php } ?>
+</div>
 
 <div>
     <a href="form_project-add.php">
