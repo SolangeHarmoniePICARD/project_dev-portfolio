b5a469d32f0c232b2e4a21573a1481be3ceb2a2d
bdebot-dev <b.debot@codeur.online>
2022-04-12 21:25:03 +0200

    database connected successfully

---
+++ db_connexion.php
@@ -0,0 +1,15 @@
+
+<?php
+$db_servername = "localhost";
+$db_dbname = "db_project-portfolio";
+$db_username = "bdebot-dev";
+$db_password = "xifulibr";
+
+try {
+    $db = new PDO("mysql:host=$db_servername;dbname=$db_dbname", $db_username, $db_password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
