d98129b5a3d8ab55869dc4d68693a7387e659b1c
bdebot-dev <b.debot@codeur.online>
2022-04-12 22:17:41 +0200

    Reorganization: Adding a form page

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php include 'header.php' ?>
 
-<form action="handler_project-add.php" method="post">
-    <div>
-        <label for="input_title">Title: </label>
-        <input type="text" id="input_title" name="data_title">
-    </div>
-    <div>
-        <label for="input_description">Description: </label>
-        <textarea id="input_description" rows="8" name="data_description"></textarea>
-    </div>
-    <div >
-        <input type="submit" id="form_submit" value="Add Project">
-        <input type="reset" value="Reset">
-    </div>
-</form>
+<div>
+    <a href="form_project-add.php">
+        <button>Add project</button>
+    </a>
+</div>
 
 
 <?php include 'footer.php' ?>
