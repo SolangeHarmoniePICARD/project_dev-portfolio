824076d1103b55881b89553ef471c191e7c1b0ed
bdebot-dev <bdebot-dev@outlook.com>
2022-05-22 13:26:18 +0200

    Token validation verification when user connection.

--- handler-user_login.php
+++ handler-user_login.php
@@ -14,22 +14,38 @@ if(isset($_POST['data_username']) && !empty($_POST['data_username'])
 
     require_once('db_close.php');
 
-    if(!$result){
+    if (!$result){
+
         $_SESSION['message'] = 'Username or password are incorrect.';
         header('Location: form-user_login.php'); 
-    }else{
+
+    } else if ($result['user_status'] == 0) {
+
+        $_SESSION['message'] = 'You must click on the link you received by email to activate your account.';
+        header('Location: form-user_login.php'); 
+
+    } else {
+
         $checking_password = password_verify($_POST['data_password'], $result['user_password']);
+
         if (!$checking_password) {
+
             $_SESSION['message'] = 'Username or password are incorrect.';
             header('Location: form-user_login.php');
+
         }else{
+
             $_SESSION['id'] = $result['user_id'];
             $_SESSION['username'] = $result['user_username'];
             $_SESSION['message'] = 'Successs!';
-            header('Location: view-backoffice_home.php');  
+            header('Location: view-backoffice_home.php');
+
         } 
     }
+
 } else {
+
     $_SESSION['message'] = 'Complete all form fields.';
     header('Location: form-user_login.php'); 
+
 }
